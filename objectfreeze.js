eacade5f6f5c44793e6dfcad831c9bdfd6b7e261
Mahigavale <maheshgavale07@gmail.com>
2024-03-07 18:44:28 +0530

    the freeze method in the js

--- objectfreeze.js
+++ objectfreeze.js
@@ -22,3 +22,47 @@ Object.freeze(student);
 //     value:123456
 // })
 // console.log(student.pincode2);
+
+
+/*
+remember the fact that the freeze is a shallow
+freeze method. which menas that if the object properties
+which are not primitive then we can modify or change the
+non primitive properties of the given object.
+*/
+
+
+let employee=
+{
+    name:'mahesh',
+    mobile:1234567890,
+    address:
+    {
+        city:'Pune',
+        pin:223344,
+        locality:'Hadapsar'
+    }
+}
+
+console.log(employee);
+
+console.log("freeze line is here:",Object.freeze(employee));
+//freeze returns the object it took as argument.
+
+console.log(employee.mobile=78687989);
+console.log(employee.name="sachin");
+console.log(employee.address.city="latur");
+console.log(employee.address.locality="tution area");
+console.log(employee);
+
+/*
+we can change the property of the object if the property is
+also object.
+that is the inner objects are not covered by
+the object.freeze method.
+*/
+
+/*
+to use the freeze the inner objects in the object 
+use the deep freeze.
+*/
