d842b65f04b118b9ef8e931a8502ad0f79ba922d
Mahigavale <maheshgavale07@gmail.com>
2024-03-07 17:45:59 +0530

    the define properties method in js

--- defineproperties.js
+++ defineproperties.js
@@ -37,13 +37,59 @@ Object.defineProperties(obj,{
     }
 })
 
-console.log(obj.property1);
+//console.log(obj.property1);
 obj.property1="asdfg";
-console.log(obj.property2);
+//console.log(obj.property2);
 obj.property2='asdfgjkjhgfdsa';
-console.log(obj.property1);
-console.log(obj.property2);
-console.log(delete obj.property2);
-console.log(obj);
+//console.log(obj.property1);
+//console.log(obj.property2);
+//console.log(delete obj.property2);
+//console.log(obj);
 //the define properties can be used to define the multiple
 //proprties at the single go.
+
+
+let profile={
+    name:'mahesh',
+    city:'Pune',
+    pincode:123456
+}
+console.log(profile);
+
+Object.defineProperties(profile,{
+    taluka:
+    {
+        value:'Lohara',
+        writable:true,
+        configurable:true
+    },
+    district:
+    {
+        value:'dharashiv',
+        writable:true,
+        configurable:true
+    }
+})
+
+console.log(profile);
+console.log(profile.taluka);
+console.log(profile.district);
+
+console.log(Object.getOwnPropertyDescriptor(profile,'taluka'));
+console.log(Object.getOwnPropertyDescriptor(profile,'district'));
+console.log(Object.getOwnPropertyDescriptors(profile,'taluka','district'));
+console.log("the taluka property before:",profile.taluka);
+
+Object.defineProperty(profile,'taluka',
+{
+    value:'Omerga',
+    writable:true,
+    configurable:true,
+    enumerable:true
+
+})
+console.log("the taluka property after change:",profile.taluka);
+console.log(Object.getOwnPropertyDescriptor(profile,'taluka'));
+console.log(Object.getOwnPropertyDescriptors(profile,'taluka','district'));
+//data descriptors: value and writable
+//access descriptors:configurable , enumerable, get ,set
